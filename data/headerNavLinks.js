6001f47f2a00630480771f1988042686ae49fb3d
TuanDev <kutuanonline199@gmail.com>
2023-02-19 11:11:43 +0700

    Fix run build project

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,9 +1,6 @@
 const headerNavLinks = [
-  // { href: '/learning', title: 'Learning' },
   { href: '/blog', title: 'Blog' },
-  // { href: '/projects', title: 'Projects' },
   { href: '/snippets', title: 'Snippets' },
-  // { href: '/about', title: 'About' },
 ]
 
 export default headerNavLinks
